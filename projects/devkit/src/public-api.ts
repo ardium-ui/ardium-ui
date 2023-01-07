e2df8009f4ea1d21990509634f6f5996c2ef49a6
joker876 <42680429+joker876@users.noreply.github.com>
2023-01-07 17:17:01 +0100

    create click-outside module/directive

--- projects/devkit/src/public-api.ts
+++ projects/devkit/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of devkit
  */
 
+export * from "./lib/click-outside/click-outside.module";
+export * from "./lib/click-outside/click-outside.directive";
+
 export * from "./lib/coercion/boolean";
 export * from "./lib/coercion/number";
 export * from "./lib/coercion/array";
