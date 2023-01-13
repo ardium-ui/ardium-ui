38a90ac800d93f1c5b3e3231810f3d2006d3393e
joker876 <42680429+joker876@users.noreply.github.com>
2023-01-13 20:03:04 +0100

    create item storage classes & types

---
+++ projects/ui/src/lib/_internal/item-storages/item-storage.types.ts
@@ -0,0 +1,74 @@
+
+/**
+```typescript
+interface ArdOption {
+    readonly itemData: any;
+    readonly index: number;
+    readonly label: string;
+    readonly value: any;
+    disabled?: boolean;
+    highlighted?: boolean;
+    selected?: boolean;
+}
+```
+ */
+export interface ArdOptionSimple {
+    readonly itemData: any;
+    readonly index: number;
+    readonly label: string;
+    readonly value: any;
+    disabled?: boolean;
+    selected?: boolean;
+    highlighted?: boolean;
+}
+/**
+```typescript
+interface ArdOption {
+    readonly itemData: any;
+    readonly index: number;
+    readonly label: string;
+    readonly value: any;
+    readonly group?: any;
+    disabled?: boolean;
+    selected?: boolean;
+    highlighted?: boolean;
+    highlighted_recently?: boolean;
+}
+```
+ */
+export interface ArdOption extends ArdOptionSimple {
+    readonly group?: any;
+    highlighted_recently?: boolean;
+}
+
+/**
+```typescript
+interface ArdItemGroup {
+    label: string;
+    disabled?: boolean;
+    highlighted?: boolean;
+    selected?: boolean;
+    children: ArdOption[];
+}
+```
+ */
+export interface ArdOptionGroup {
+    label: string;
+    disabled?: boolean;
+    highlighted?: boolean;
+    selected?: boolean;
+    children: ArdOption[];
+}
+
+export const ArdPanelPosition = {
+    Top: 'top',
+    Bottom: 'bottom',
+    Auto: 'auto',
+} as const;
+export type ArdPanelPosition = typeof ArdPanelPosition[keyof typeof ArdPanelPosition];
+
+export type ArdItemGroupMap = Map<any, ArdOptionGroup>;
+
+export type GroupByFn = (item: any) => any;
+export type SearchFn = (searchTerm: string, item: ArdOption) => boolean;
+export type CompareWithFn = (value: any, optionValue: any) => boolean;
---
+++ projects/ui/src/lib/_internal/item-storages/simple-item-storage.ts
@@ -0,0 +1,254 @@
+import { ArdOptionSimple, CompareWithFn } from "./item-storage.types";
+import resolvePath from 'resolve-object-path';
+import { any, evaluate, isDefined, isObject, isPrimitive } from "simple-bool";
+
+export interface ItemStorageHostDefaults {
+    valueFrom: string;
+    labelFrom: string;
+    disabledFrom: string;
+    [key: string]: any;
+}
+export interface SimpleItemStorageHost {
+    valueFrom?: string;
+    labelFrom?: string;
+    disabledFrom?: string;
+    invertDisabled?: boolean;
+    readonly DEFAULTS: ItemStorageHostDefaults;
+    compareWith?: CompareWithFn;
+    multiselectable: boolean;
+    maxSelectedItems?: number;
+}
+
+export class SimpleItemStorage {
+    private _items: ArdOptionSimple[] = [];
+    private _highlightedItems: ArdOptionSimple[] = [];
+    private _selectedItems: ArdOptionSimple[] = [];
+
+    constructor(
+        private _ardParentComp: SimpleItemStorageHost,
+    ) {}
+
+    get items(): ArdOptionSimple[] {
+        return this._items;
+    }
+    get selectedItems(): ArdOptionSimple[] {
+        return this._selectedItems;
+    }
+    get highlightedItems(): ArdOptionSimple[] {
+        return this._highlightedItems;
+    }
+    get value(): any[] {
+        return this._itemsToValue(this.selectedItems);
+    }
+    private _itemsToValue(items: ArdOptionSimple[]): any[] {
+        return items.map(item => item.value);
+    }
+
+    get isAnyItemHighlighted(): boolean {
+        return this._highlightedItems.length > 0;
+    }
+    get isItemLimitReached(): boolean {
+        if (!this._ardParentComp.multiselectable || !isDefined(this._ardParentComp.maxSelectedItems)) {
+            return false;
+        }
+        return this._ardParentComp.maxSelectedItems <= this.selectedItems.length;
+    }
+
+    setItems(items: any[]) {
+        let areItemsPrimitive = false;
+        if (any(items, isPrimitive)) {
+            items = items.map(this._primitiveItemsMapFn);
+            areItemsPrimitive = true;
+        }
+
+        this._items = items.map((item, index) => {
+            return this._setItemsMapFn(item, index, areItemsPrimitive);
+        });
+
+        return areItemsPrimitive;
+    }
+    private _primitiveItemsMapFn<T>(item: T): { value: T } {
+        return { value: item };
+    }
+    private _setItemsMapFn(itemData: any, index: number, areItemsPrimitive: boolean): ArdOptionSimple {
+        if (areItemsPrimitive) {
+            return {
+                itemData,
+                index,
+                value: itemData.value,
+                label: itemData.value?.toString?.() ?? String(itemData.value),
+            }
+        }
+        //get value
+        const valuePath = this._ardParentComp.valueFrom ?? this._ardParentComp.labelFrom ?? this._ardParentComp.DEFAULTS.valueFrom;
+        const value = resolvePath(itemData, valuePath);
+
+        //get label
+        const labelPath = this._ardParentComp.labelFrom ?? this._ardParentComp.valueFrom ?? this._ardParentComp.DEFAULTS.labelFrom;
+        const label = resolvePath(itemData, labelPath) ?? value;
+
+        //get disabled
+        const disabledPath = this._ardParentComp.disabledFrom ?? this._ardParentComp.DEFAULTS.disabledFrom;
+        let disabled = evaluate(resolvePath(itemData, disabledPath));
+        if (this._ardParentComp.invertDisabled) {
+            disabled = !disabled;
+        }
+
+        //return
+        return {
+            itemData,
+            index,
+            value,
+            label: label?.toString?.() ?? String(label),
+            disabled,
+        }
+    }
+    private _isWriteValueValid(ngModel: any[]): boolean {
+        return ngModel.every(item => {
+            if (!isDefined(this._ardParentComp.compareWith) && isObject(item) && this._ardParentComp.valueFrom) {
+                console.warn(
+                    `Setting object(${JSON.stringify(item)}) as your model with [valueFrom] is not allowed unless [compareWith] is used.`
+                );
+                return false;
+            }
+            return true;
+        });
+    }
+    handleWriteValue(ngModel: any[]): void {
+        this.clearAllSelected();
+
+        if (!this._isWriteValueValid(ngModel)) {
+            return;
+        }
+
+        const selectItemByValue = (value: any) => {
+            let item = this.findItemByValue(value);
+
+            if (item) {
+                this.selectItem(item);
+                return;
+            }
+            console.warn(`Couldn't find an item with value ${value?.toString?.() || String(value)}.`);
+        }
+
+        for (const modelValue of ngModel) {
+            selectItemByValue(modelValue);
+        }
+    }
+    findItemByValue(valueToFind: any): ArdOptionSimple | undefined {
+        let findBy: (item: ArdOptionSimple) => boolean;
+        if (this._ardParentComp.compareWith) {
+            findBy = item => this._ardParentComp.compareWith!(valueToFind, item.value);
+        }
+        else {
+            findBy = item => item.value === valueToFind;
+        }
+        return this._items.find(item => findBy(item));
+    }
+
+    clearAllSelected(): any[] {
+        for (const item of this._selectedItems) {
+            item.selected = false;
+        }
+        let ret = this._selectedItems.map(item => item.value);
+
+        return ret;
+    }
+    selectItem(...items: ArdOptionSimple[]): [any[], any[], any[]] {
+        if (this.isItemLimitReached) {
+            return [[], [], items.map(item => item.value)];
+        }
+        let unselected = [];
+        if (!this._ardParentComp.multiselectable) {
+            unselected = this.clearAllSelected();
+        }
+
+        let itemsSelectedCount = 0;
+        const itemsSelected = [];
+        for (const item of items) {
+            itemsSelectedCount++;
+            if (item.selected) continue;
+            if (this.isItemLimitReached) {
+                break;
+            }
+            item.selected = true;
+            this._selectedItems.push(item);
+            itemsSelected.push(item);
+        }
+
+        const itemsFailedToSelect = items.slice(itemsSelectedCount - 1);
+        return [itemsSelected.map(item => item.value), unselected, itemsFailedToSelect.map(item => item.value)];
+    }
+    unselectItem(...items: ArdOptionSimple[]): any[] {
+        for (const item of items) {
+            if (!item.selected) continue;
+            item.selected = false;
+        }
+        this._selectedItems = this._selectedItems.filter(v => v.selected);
+
+        return items.map(item => item.value);
+    }
+
+    clearAllHighlights(): void {
+        for (const item of this._highlightedItems) {
+            item.highlighted = false;
+        }
+        this._highlightedItems = [];
+    }
+    highlightSingleItem(item: ArdOptionSimple): ArdOptionSimple | null {
+        if (!item || item.disabled) return null;
+        this.clearAllHighlights();
+        return this.highlightItem(item);
+    }
+    highlightItem(...items: ArdOptionSimple[]): ArdOptionSimple {
+        for (const item of items) {
+            item.highlighted = true;
+        }
+        this._highlightedItems.push(...items);
+        return items.last();
+    }
+    unhighlightItem(...items: ArdOptionSimple[]): void {
+        for (const item of items) {
+            if (!item || !item.highlighted) return;
+
+            item.highlighted = false;
+        }
+        this._highlightedItems = this._highlightedItems.filter(v => v.highlighted);
+    }
+    highlightFirstItem(): ArdOptionSimple | null {
+        this.clearAllHighlights();
+
+        let itemToHighlight = this._items.first(1, item => !item.disabled);
+        return this.highlightItem(itemToHighlight);
+    }
+    highlightAllItems(): void {
+        let itemsToHighlight = this._items.filter(item => !item.disabled);
+
+        this.highlightItem(...itemsToHighlight);
+    }
+    highlightNextItem(offset: number, hasShift?: boolean): ArdOptionSimple | null {
+        if (!this.isAnyItemHighlighted) {
+            return this.highlightFirstItem();
+        }
+        const currentItem = this.highlightedItems.last();
+        const itemsWithoutDisabled = this._items.filter(item => !item.disabled);
+        const currentIndexInItems = itemsWithoutDisabled.findIndex(item => item.index == currentItem.index);
+
+        let nextItemIndex = currentIndexInItems + offset;
+        if (nextItemIndex >= itemsWithoutDisabled.length) {
+            nextItemIndex -= itemsWithoutDisabled.length;
+        }
+        if (nextItemIndex < 0) {
+            nextItemIndex += itemsWithoutDisabled.length;
+        }
+        const itemToHighlight = itemsWithoutDisabled[nextItemIndex];
+
+        if (hasShift && this._ardParentComp.multiselectable) {
+            if (itemToHighlight.highlighted) {
+                this.unhighlightItem(currentItem);
+            }
+            return this.highlightItem(itemToHighlight);
+        }
+        return this.highlightSingleItem(itemToHighlight);
+    }
+}
