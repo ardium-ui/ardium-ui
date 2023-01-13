535c05ec7424f3f55f6a3385b92d2a535d1b86c9
joker876 <42680429+joker876@users.noreply.github.com>
2023-01-13 19:36:46 +0100

    create clear button component (internal only)

---
+++ projects/ui/src/lib/_internal/clear-button/clear-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { _FocusableComponent } from './../focusable-component';
+
+@Component({
+  selector: 'ard-_clear-button',
+  templateUrl: './clear-button.component.html',
+  styleUrls: ['./clear-button.component.scss']
+})
+export class _ClearButtonComponent extends _FocusableComponent {
+    
+}
---
+++ projects/ui/src/lib/_internal/clear-button/clear-button.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { _ClearButtonComponent } from './clear-button.component';
+
+
+
+@NgModule({
+    declarations: [
+        _ClearButtonComponent,
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        _ClearButtonComponent,
+    ]
+})
+export class _ClearButtonModule { }
