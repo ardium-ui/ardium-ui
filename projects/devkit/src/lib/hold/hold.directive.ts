b643777f4f7493d6933f62049cf405440ac27aa3
joker876 <joker876x@gmail.com>
2023-03-26 23:18:22 +0200

    [devkit] fix hold directive

--- projects/devkit/src/lib/hold/hold.directive.ts
+++ projects/devkit/src/lib/hold/hold.directive.ts
@@ -7,24 +7,33 @@ import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/c
 export class HoldDirective {
 
     @Output('ardHold')
-    public holdEvent = new EventEmitter<null>();
+    public holdEvent = new EventEmitter<number>();
 
-    @Input('ardHoldDelay') holdDelay: number = 800;
-    @Input('ardHoldRepeat') holdRepeat: number = 100;
+    @Input('ardHoldDelay') holdDelay: number = 500;
+    @Input('ardHoldRepeat') holdRepeat: number = 1000/15;
 
     interval: NodeJS.Timeout | null = null;
+    timeout: NodeJS.Timeout | null = null;
 
-    @HostListener('document:mousedown')
-    @HostListener('document:touchstart')
+    @HostListener('mousedown')
+    @HostListener('touchstart')
     public onMouseDown(): void {
-        this.interval = setInterval(() => {
-            this.holdEvent.next(null);
-        }, this.holdRepeat);
+        this.timeout = setTimeout(() => {
+            this.timeout = null;
+            this.interval = setInterval(() => {
+                this.holdEvent.next(Math.random());
+            }, this.holdRepeat);
+        }, this.holdDelay);
     }
 
-    @HostListener('document:mouseup')
-    @HostListener('document:touchend')
+    @HostListener('mouseup')
+    @HostListener('touchend')
     public onMouseUp(): void {
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+            this.timeout = null;
+            return;
+        }
         if (!this.interval) return;
 
         clearInterval(this.interval);
