555c2bf23bac69e53cb92310d36ce9d3cd19d036
joker876 <joker876x@gmail.com>
2023-04-04 20:57:38 +0200

    split segment component into _SelectableListComponent

---
+++ projects/ui/src/lib/_internal/selectable-list-component.ts
@@ -0,0 +1,305 @@
+
+import { ChangeDetectorRef, Directive, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+import { coerceArrayProperty, coerceBooleanProperty, coerceNumberProperty } from '@ardium-ui/devkit';
+import { ArdOptionSimple, OptionContext } from './item-storages/item-storage.types';
+import { SimpleItemStorage, SimpleItemStorageHost } from './item-storages/simple-item-storage';
+import { _NgModelComponent } from './ngmodel-component';
+
+@Directive()
+export abstract class _SelectableListComponent extends _NgModelComponent implements ControlValueAccessor, SimpleItemStorageHost {
+
+    //! public constants
+    readonly itemStorage = new SimpleItemStorage(this);
+    readonly htmlId = crypto.randomUUID();
+    readonly element!: HTMLElement;
+    readonly DEFAULTS = {
+        valueFrom: 'value',
+        labelFrom: 'label',
+        disabledFrom: 'disabled',
+    }
+
+    constructor(
+        private _cd: ChangeDetectorRef,
+    ) {
+        super();
+    }
+
+    //! binding-related inputs
+    @Input() valueFrom?: string;
+    @Input() labelFrom?: string;
+    @Input() disabledFrom?: string;
+
+
+    //! items setter/getter
+    @Input()
+    get items(): any[] { return this.itemStorage.items }
+    set items(value: any) {
+        if (!Array.isArray(value)) value = coerceArrayProperty(value);
+
+        let shouldPrintErrors = this.itemStorage.setItems(value);
+
+        if (shouldPrintErrors) {
+            this._printPrimitiveWarnings();
+        }
+    };
+    private _printPrimitiveWarnings() {
+        function makeWarning(str: string): void {
+            console.warn(`Skipped using [${str}] property bound to <ard-segment>, as some provided items are of primitive type`);
+        }
+        if (this.valueFrom) {
+            makeWarning('valueFrom');
+        }
+        if (this.labelFrom) {
+            makeWarning('labelFrom');
+        }
+        if (this.disabledFrom) {
+            makeWarning('disabledFrom');
+        }
+        if (this.invertDisabled) {
+            makeWarning('invertDisabled');
+        }
+    }
+
+    //! multiselectable
+    protected _multiselectable: boolean = false;
+    abstract multiselectable: any;
+    get singleselectable(): boolean { return !this._multiselectable };
+
+    //! coerced properties
+    //should the value that the "disabledFrom" path lead to be inverted?
+    //useful when the property is e.g. "active", which is the oposite of "disabled"
+    private _invertDisabled: boolean = false;
+    @Input()
+    get invertDisabled(): boolean { return this._invertDisabled; }
+    set invertDisabled(v: any) { this._invertDisabled = coerceBooleanProperty(v); }
+
+    private _maxSelectedItems: number | undefined = undefined;
+    @Input()
+    get maxSelectedItems(): number | undefined { return this._maxSelectedItems; }
+    set maxSelectedItems(v: any) { this._maxSelectedItems = coerceNumberProperty(v); }
+
+    //! control value accessor
+    //override the writeValue and setDisabledState defined in _NgModelComponent
+    override setDisabledState(state: boolean): void {
+        this._disabled = state;
+        this._cd.markForCheck();
+    }
+    writeValue(ngModel: any[]): void {
+        this.itemStorage.handleWriteValue(ngModel);
+        this._cd.markForCheck();
+    }
+
+    //! change & touch event emitters
+    private _touched: boolean = false;
+    @HostBinding('class.ard-touched')
+    get touched(): boolean { return this._touched };
+    protected set touched(state: boolean) {
+        this._touched = state;
+    }
+
+    protected _emitChanges(): void {
+        let value = this.itemStorage.value;
+        this._onChangeRegistered?.(value);
+        this.changeEvent.emit(value);
+        this.valueChange.emit(value);
+    }
+    protected _onTouched(): void {
+        if (this.touched) return;
+
+        this._onTouchedRegistered?.();
+        this.touched = true;
+    }
+
+    //! focus & blur handlers
+    lastBlurTimestamp: number | null = null;
+    override onFocus(event: FocusEvent): void {
+        super.onFocus(event);
+
+        if (this.touched || !this.lastBlurTimestamp || this.lastBlurTimestamp + 1 < Date.now()) return;
+        this.lastBlurTimestamp = null;
+
+        this._onTouched();
+    }
+    override onBlur(event: FocusEvent): void {
+        super.onBlur(event);
+
+        if (!this.touched) this.lastBlurTimestamp = Date.now();
+    }
+
+    //! getters
+    get firstHighlightedItem(): ArdOptionSimple | undefined {
+        return this.itemStorage.highlightedItems?.first();
+    }
+    get highlightedItems(): ArdOptionSimple[] {
+        return this.itemStorage.highlightedItems;
+    }
+    get isItemLimitReached(): boolean {
+        return this.itemStorage.isItemLimitReached;
+    }
+
+    //! context providers
+    getOptionContext(item: ArdOptionSimple): OptionContext {
+        return {
+            $implicit: item,
+            item,
+            itemData: item.itemData,
+        }
+    }
+
+    //! value input & output
+    @Input()
+    set value(newValue: any) {
+        if (!Array.isArray(newValue)) newValue = coerceArrayProperty(newValue);
+        this.writeValue(newValue);
+    }
+    @Output() valueChange = new EventEmitter<any[]>();
+
+    //! output events
+    @Output('change') changeEvent = new EventEmitter<any[]>();
+    @Output('add') addEvent = new EventEmitter<any[]>();
+    @Output('remove') removeEvent = new EventEmitter<any[]>();
+
+
+    //! item selection handlers
+    toggleItem(item: ArdOptionSimple): void {
+        if (item.selected) {
+            this.unselectItem(item);
+            return;
+        }
+        this.selectItem(item);
+    }
+    selectItem(...items: ArdOptionSimple[]): void {
+        let [selected, unselected] = this.itemStorage.selectItem(...items);
+
+        if (unselected.length > 0) this.removeEvent.emit(unselected);
+
+        if (selected.length > 0) {
+            this.addEvent.emit(selected);
+            this._emitChanges();
+        }
+    }
+    unselectItem(...items: ArdOptionSimple[]): void {
+        let unselected = this.itemStorage.unselectItem(...items);
+
+        this.removeEvent.emit(unselected);
+        this._emitChanges();
+    }
+
+    //! highligh-related
+    private _isMouseBeingUsed: boolean = false;
+    @HostListener('mousemove')
+    onMouseMove() {
+        this._isMouseBeingUsed = true;
+    }
+    onItemMouseEnter(option: ArdOptionSimple, event: MouseEvent): void {
+        if (!this._isMouseBeingUsed) return;
+        this.itemStorage.highlightSingleItem(option);
+        event.stopPropagation();
+    }
+    onItemMouseLeave(option: ArdOptionSimple, event: MouseEvent): void {
+        if (!this._isMouseBeingUsed) return;
+        this.itemStorage.unhighlightItem(option);
+        event.stopPropagation();
+    }
+
+    //! click handlers
+    onItemClick(option: ArdOptionSimple, event: MouseEvent): void {
+        event.stopPropagation();
+        this.toggleItem(option);
+    }
+
+    //! key press handlers
+    @HostListener('keydown', ['$event'])
+    onKeyPress(event: KeyboardEvent): void {
+        switch (event.code) {
+            case 'Space':
+            case 'Enter': {
+                this._toggleHighlightedItems(event);
+                return;
+            }
+            case 'ArrowRight': {
+                this._highlightNext(event);
+                return;
+            }
+            case 'ArrowLeft': {
+                this._highlightPrevious(event);
+                return;
+            }
+            case 'Home': {
+                this._highlightFirst(event);
+                return;
+            }
+            case 'End': {
+                this._highlightLast(event);
+                return;
+            }
+            case 'Tab': {
+                this._skipToNextElement(event.shiftKey);
+                return;
+            }
+            case 'KeyA': {
+                if (event.ctrlKey) {
+                    this._highlightAll(event);
+                    return;
+                }
+            }
+        }
+    }
+    private _toggleHighlightedItems(event: KeyboardEvent): void {
+        if (!this.isFocused) return;
+
+        event.preventDefault();
+
+        const highlightedItems = this.highlightedItems;
+
+        if (highlightedItems.every(item => item.selected)) {
+            this.unselectItem(...highlightedItems);
+        }
+        else {
+            this.selectItem(...highlightedItems);
+        }
+    }
+    private _highlightPrevious(event: KeyboardEvent): void {
+        if (!this.isFocused) return;
+
+        event.preventDefault();
+        this._isMouseBeingUsed = false;
+
+        this.itemStorage.highlightNextItem(-1, event.shiftKey);
+    }
+    private _highlightNext(event: KeyboardEvent): void {
+        if (!this.isFocused) return;
+
+        event.preventDefault();
+        this._isMouseBeingUsed = false;
+
+        this.itemStorage.highlightNextItem(+1, event.shiftKey);
+    }
+    private _highlightFirst(event: KeyboardEvent): void {
+        if (!this.isFocused) return;
+
+        event.preventDefault();
+        this._isMouseBeingUsed = false;
+
+        this.itemStorage.highlightFirstItem();
+    }
+    private _highlightLast(event: KeyboardEvent): void {
+        if (!this.isFocused) return;
+
+        event.preventDefault();
+        this._isMouseBeingUsed = false;
+
+        this.itemStorage.highlightLastItem();
+    }
+    private _highlightAll(event: KeyboardEvent): void {
+        this.itemStorage.highlightAllItems();
+    }
+    private _skipToNextElement(hasShift: boolean): void {
+        if (hasShift) {
+            this.focusFirst();
+            return;
+        }
+        this.focusLast();
+    }
+}
--- projects/ui/src/lib/segment/segment.component.ts
+++ projects/ui/src/lib/segment/segment.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, EventEmitter, HostBinding, HostListener, Input, Output, TemplateRef, ViewEncapsulation } from '@angular/core';
-import { coerceArrayProperty, coerceBooleanProperty, coerceNumberProperty } from '@ardium-ui/devkit';
+import { ChangeDetectionStrategy, Component, ContentChild, HostBinding, Input, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { coerceBooleanProperty, coerceNumberProperty } from '@ardium-ui/devkit';
 import { ComponentColor } from '../types/colors.types';
-import { ArdOption, ArdOptionSimple, OptionContext } from '../_internal/item-storages/item-storage.types';
-import { _NgModelComponent } from '../_internal/ngmodel-component';
-import { SimpleItemStorage, SimpleItemStorageHost } from './../_internal/item-storages/simple-item-storage';
+import { ArdOptionSimple } from '../_internal/item-storages/item-storage.types';
+import { _SelectableListComponent } from '../_internal/selectable-list-component';
+import { SimpleItemStorageHost } from './../_internal/item-storages/simple-item-storage';
 import { ArdSegmentOptionTemplateDirective } from './segment.directives';
 import { SegmentAppearance, SegmentVariant } from './segment.types';
 
@@ -19,28 +19,7 @@ type SegmentRow = {
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ArdiumSegmentComponent extends _NgModelComponent implements SimpleItemStorageHost {
-
-    //! public constants
-    readonly itemStorage = new SimpleItemStorage(this);
-    readonly htmlId = crypto.randomUUID();
-    readonly element!: HTMLElement;
-    readonly DEFAULTS = {
-        valueFrom: 'value',
-        labelFrom: 'label',
-        disabledFrom: 'disabled',
-    }
-
-    constructor(
-        private _cd: ChangeDetectorRef,
-    ) {
-        super();
-    }
-
-    //! binding-related inputs
-    @Input() valueFrom?: string;
-    @Input() labelFrom?: string;
-    @Input() disabledFrom?: string;
+export class ArdiumSegmentComponent extends _SelectableListComponent implements SimpleItemStorageHost {
 
     //! appearance
     @Input() appearance: SegmentAppearance = SegmentAppearance.Outlined;
@@ -55,43 +34,12 @@ export class ArdiumSegmentComponent extends _NgModelComponent implements SimpleI
         ].join(' ');
     }
 
-    //! items setter/getter
-    @Input()
-    get items(): any[] { return this.itemStorage.items }
-    set items(value: any[] | null) {
-        if (value == null) value = [];
-        let shouldPrintErrors = this.itemStorage.setItems(value);
-
-        this._itemRowsCache = null;
-
-        if (shouldPrintErrors) {
-            this._printPrimitiveWarnings();
-        }
-    };
-
     //! coerced properties
-    //should the value that the "disabledFrom" path lead to be inverted?
-    //useful when the property is e.g. "active", which is the oposite of "disabled"
-    private _invertDisabled: boolean = false;
-    @Input()
-    get invertDisabled(): boolean { return this._invertDisabled; }
-    set invertDisabled(v: any) { this._invertDisabled = coerceBooleanProperty(v); }
-
-    private _multiselectable: boolean = false;
     @Input()
     @HostBinding('attr.multiple')
     @HostBinding('class.ard-multiselect')
     get multiselectable(): boolean { return this._multiselectable };
     set multiselectable(v: any) { this._multiselectable = coerceBooleanProperty(v); }
-    @HostBinding('class.ard-singleselect')
-    get singleselectable(): boolean { return !this._multiselectable };
-
-    private _touched: boolean = false;
-    @HostBinding('class.ard-touched')
-    get touched(): boolean { return this._touched };
-    private set touched(state: boolean) {
-        this._touched = state;
-    }
 
     private _autoFocus: boolean = false;
     @Input()
@@ -112,111 +60,16 @@ export class ArdiumSegmentComponent extends _NgModelComponent implements SimpleI
         this._itemsPerRow = newValue;
     }
     get itemsInActualRow(): number { return this._itemsPerRow ?? this.items.length }
-
-    private _maxSelectedItems: number | undefined = undefined;
-    @Input()
-    get maxSelectedItems(): number | undefined { return this._maxSelectedItems; }
-    set maxSelectedItems(v: any) { this._maxSelectedItems = coerceNumberProperty(v); }
     
 
     //! option template
     @ContentChild(ArdSegmentOptionTemplateDirective, { read: TemplateRef }) optionTemplate?: TemplateRef<any>;
-
-    //! control value accessor
-    //override the writeValue and setDisabledState defined in _NgModelComponent
-    override setDisabledState(state: boolean): void {
-        this._disabled = state;
-        this._cd.markForCheck();
-    }
-    writeValue(ngModel: any[]): void {
-        this.itemStorage.handleWriteValue(ngModel);
-        this._cd.markForCheck();
-    }
-    //! change & touch event emitters
-    private _emitChanges(): void {
-        let value = this.itemStorage.value;
-        this._onChangeRegistered?.(value);
-        this.changeEvent.emit(value);
-        this.valueChange.emit(value);
-    }
-    private _onTouched(): void {
-        if (this.touched) return;
-
-        this._onTouchedRegistered?.();
-        this.touched = true;
-    }
-
-    //! focus & blur handlers
-    lastBlurTimestamp: number | null = null;
-    override onFocus(event: FocusEvent): void {
-        super.onFocus(event);
-
-        if (this.touched || !this.lastBlurTimestamp || this.lastBlurTimestamp + 1 < Date.now()) return;
-        this.lastBlurTimestamp = null;
-
-        this._onTouched();
-    }
-    override onBlur(event: FocusEvent): void {
-        super.onBlur(event);
-
-        if (!this.touched) this.lastBlurTimestamp = Date.now();
-    }
-
-    //! value input & output
-    @Input()
-    set value(newValue: any) {
-        if (!Array.isArray(newValue)) newValue = coerceArrayProperty(newValue);
-        this.writeValue(newValue);
-    }
-    @Output() valueChange = new EventEmitter<any[]>();
-
-    //! output events
-    @Output('change') changeEvent = new EventEmitter<any[]>();
-    @Output('add') addEvent = new EventEmitter<any[]>();
-    @Output('remove') removeEvent = new EventEmitter<any[]>();
-
-    //! context providers
-    getOptionContext(item: ArdOption): OptionContext {
-        return {
-            $implicit: item,
-            item,
-            itemData: item.itemData,
-        }
-    }
 
     //! lifecycle hooks
     ngAfterContentInit(): void {
         if (this.autoFocus) {
             this.focus();
         }
-    }
-    private _printPrimitiveWarnings() {
-        function makeWarning(str: string): void {
-            console.warn(`Skipped using [${str}] property bound to <ard-segment>, as some provided items are of primitive type`);
-        }
-        if (this.valueFrom) {
-            makeWarning('valueFrom');
-        }
-        if (this.labelFrom) {
-            makeWarning('labelFrom');
-        }
-        if (this.disabledFrom) {
-            makeWarning('disabledFrom');
-        }
-        if (this.invertDisabled) {
-            makeWarning('invertDisabled');
-        }
-    }
-
-    //! getters
-    get firstHighlightedItem(): ArdOptionSimple | undefined {
-        return this.itemStorage.highlightedItems?.first();
-    }
-    get highlightedItems(): ArdOptionSimple[] {
-        return this.itemStorage.highlightedItems;
-    }
-    get isItemLimitReached(): boolean {
-        return this.itemStorage.isItemLimitReached;
     }
 
     //! item row getters
@@ -247,147 +100,5 @@ export class ArdiumSegmentComponent extends _NgModelComponent implements SimpleI
 
         this._itemRowsCache = itemRows;
         return itemRows;
-    }
-
-    //! item selection handlers
-    toggleItem(item: ArdOption): void {
-        if (item.selected) {
-            this.unselectItem(item);
-            return;
-        }
-        this.selectItem(item);
-    }
-    selectItem(...items: ArdOption[]): void {
-        let [selected, unselected] = this.itemStorage.selectItem(...items);
-
-        if (unselected.length > 0) this.removeEvent.emit(unselected);
-
-        if (selected.length > 0) {
-            this.addEvent.emit(selected);
-            this._emitChanges();
-        }
-    }
-    unselectItem(...items: ArdOption[]): void {
-        let unselected = this.itemStorage.unselectItem(...items);
-
-        this.removeEvent.emit(unselected);
-        this._emitChanges();
-    }
-
-    //! highligh-related
-    private _isMouseBeingUsed: boolean = false;
-    @HostListener('mousemove')
-    onMouseMove() {
-        this._isMouseBeingUsed = true;
-    }
-    onItemMouseEnter(option: ArdOption, event: MouseEvent): void {
-        if (!this._isMouseBeingUsed) return;
-        this.itemStorage.highlightSingleItem(option);
-        event.stopPropagation();
-    }
-    onItemMouseLeave(option: ArdOption, event: MouseEvent): void {
-        if (!this._isMouseBeingUsed) return;
-        this.itemStorage.unhighlightItem(option);
-        event.stopPropagation();
-    }
-
-    //! click handlers
-    onItemClick(option: ArdOption, event: MouseEvent): void {
-        event.stopPropagation();
-        this.toggleItem(option);
-    }
-
-    //! key press handlers
-    @HostListener('keydown', ['$event'])
-    onKeyPress(event: KeyboardEvent): void {
-        switch (event.code) {
-            case 'Space':
-            case 'Enter': {
-                this._onSpaceOrEnterPress(event);
-                return;
-            }
-            case 'ArrowRight': {
-                this._onArrowRightPress(event);
-                return;
-            }
-            case 'ArrowLeft': {
-                this._onArrowLeftPress(event);
-                return;
-            }
-            case 'Home': {
-                this._onHomePress(event);
-                return;
-            }
-            case 'End': {
-                this._onEndPress(event);
-                return;
-            }
-            case 'Tab': {
-                this._onTabPress(event.shiftKey);
-                return;
-            }
-            case 'KeyA': {
-                if (event.ctrlKey) {
-                    this._onCtrlAPress(event);
-                    return;
-                }
-            }
-        }
-    }
-    private _onSpaceOrEnterPress(event: KeyboardEvent): void {
-        if (!this.isFocused) return;
-
-        event.preventDefault();
-
-        const highlightedItems = this.highlightedItems;
-
-        if (highlightedItems.every(item => item.selected)) {
-            this.unselectItem(...highlightedItems);
-        }
-        else {
-            this.selectItem(...highlightedItems);
-        }
-    }
-    private _onArrowLeftPress(event: KeyboardEvent): void {
-        if (!this.isFocused) return;
-
-        event.preventDefault();
-        this._isMouseBeingUsed = false;
-
-        this.itemStorage.highlightNextItem(-1, event.shiftKey);
-    }
-    private _onArrowRightPress(event: KeyboardEvent): void {
-        if (!this.isFocused) return;
-
-        event.preventDefault();
-        this._isMouseBeingUsed = false;
-
-        this.itemStorage.highlightNextItem(+1, event.shiftKey);
-    }
-    private _onHomePress(event: KeyboardEvent): void {
-        if (!this.isFocused) return;
-
-        event.preventDefault();
-        this._isMouseBeingUsed = false;
-
-        this.itemStorage.highlightFirstItem();
-    }
-    private _onEndPress(event: KeyboardEvent): void {
-        if (!this.isFocused) return;
-
-        event.preventDefault();
-        this._isMouseBeingUsed = false;
-
-        this.itemStorage.highlightLastItem();
-    }
-    private _onCtrlAPress(event: KeyboardEvent): void {
-        this.itemStorage.highlightAllItems();
-    }
-    private _onTabPress(hasShift: boolean): void {
-        if (hasShift) {
-            this.focusFirst();
-            return;
-        }
-        this.focusLast();
     }
 }
