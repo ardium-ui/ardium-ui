77a42506ed9e60135495c149b7000fe3b7170fdc
joker876 <joker876x@gmail.com>
2023-04-09 23:20:35 +0200

    [devkit] implement better suggestion matching algorithm

--- projects/devkit/src/lib/find-functions/find-suggestion.ts
+++ projects/devkit/src/lib/find-functions/find-suggestion.ts
@@ -1,18 +1,71 @@
 import { compareTwoStrings, findBestMatch } from "string-similarity";
 
+export function findBestSuggestions<T extends string>(toMatch: string, suggestions: Readonly<T[]>, limit: number): T[];
+export function findBestSuggestions<T extends Exclude<any, string>>(toMatch: string, suggestions: Readonly<T[]>, limit: number, mapFn: (v: T) => string): T[];
+export function findBestSuggestions<T>(toMatch: string, suggestions: Readonly<T[]>, limit: number = 7, mapFn?: (v: T) => string): T[] {
+    //map non-string values to strings, if needed
+    const suggestionStrings = suggestions.map(v => typeof v == 'string' ? v : mapFn!(v));
+    
+    //* alternative method for single-char strings
+    //the similarity algorithm always returns zero in this case
+    if (toMatch.length == 1) {
+        return suggestionStrings
+            //remember the original indexes
+            .map((v, index) => ({ str: v, index }))
+            //pick only the ones starting with the character to match
+            .filter(v => v.str.startsWith(toMatch))
+            //map to string length and index
+            .map(v => ({ rating: v.str.length, index: v.index }))
+            //sort by length, ascending
+            .sort((a, b) => a.rating - b.rating)
+            //map to original values
+            .map((v) => suggestions[v.index])
+            //set the limit
+            .slice(0, limit);
+    }
 
-export function findBestSuggestions(toMatch: string, suggestions: string[], limit: number = 8): string[] {
-    return suggestions
-        //map to values and ratings
-        .map(v => ({ target: v, rating: compareTwoStrings(toMatch, v) }))
-        //sort by rating, descending
+    console.log(suggestionStrings
+        //map to ratings of similarity between the string and toMatch
+        .map((v, index) => ({ rating: compareTwoStrings(toMatch, v), index }))
+        //sort descending
         .sort((a, b) => b.rating - a.rating)
-        //map to just values
-        .map(v => v.target)
+        //keep only ratings    above zero  &&  within 60% of max rating
+        .filter((v, _, arr) => v.rating > 0 && v.rating > arr[0].rating * 0.6)
+        //map to original values
+        .map((v) => ({...v, str: (suggestions[v.index] as any).name}))
+        //set the limit
+        .slice(0, limit));
+
+    //* standard method
+    return suggestionStrings
+        //map to ratings of similarity between the string and toMatch
+        .map((v, index) => {
+            //always include if starts with the value to match
+            if (new RegExp(`^${toMatch}`, 'i').test(v)) return { rating: 1_000 - v.length, index };
+
+            //calculate conditional similarity
+            const similarity = compareTwoStrings(toMatch, v);
+            const foundIndex = v.indexOf(toMatch);
+            const canBeFound = foundIndex != -1;
+            const foundIndexModifier = canBeFound ? (Math.cos(foundIndex / 90 * Math.PI) + 1) ** 2 / 1.5 : 1;
+            const rating = similarity * foundIndexModifier;
+            rating && console.log(rating, foundIndexModifier, String(v));
+            return {
+                rating,
+                index,
+            }
+        })
+        //keep only ratings above or equal to 0.45
+        .filter(v => v.rating >= 0.45)
+        //sort descending
+        .sort((a, b) => b.rating - a.rating)
+        //map to original values
+        .map((v) => suggestions[v.index])
         //set the limit
         .slice(0, limit);
 }
 
 export function findBestAutocomplate(toMatch: string, autocompletes: string[]): string {
-    return findBestMatch(toMatch, autocompletes).bestMatch.target;
+    const autocompletesStartingWith = autocompletes.filter(v => new RegExp(`^${toMatch}`, 'i').test(v) );
+    return findBestMatch(toMatch, autocompletesStartingWith).bestMatch.target;
 }
