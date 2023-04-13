9581bf1d2fa7062f8e96d6de9c480d2320b21d27
joker876 <joker876x@gmail.com>
2023-04-13 10:40:34 +0200

    fix slider tooltip template & css

--- projects/ui/src/lib/slider/slider.directive.ts
+++ projects/ui/src/lib/slider/slider.directive.ts
@@ -2,7 +2,7 @@ import { Directive, TemplateRef } from '@angular/core';
 import { SliderTooltipContext } from './slider.types';
 
 @Directive({
-  selector: 'ng-template[ard-slider-label]'
+  selector: 'ng-template[ard-slider-tooltip-tmp]'
 })
 export class ArdSliderTooltipDirective {
     constructor(public template: TemplateRef<SliderTooltipContext>) {  }
