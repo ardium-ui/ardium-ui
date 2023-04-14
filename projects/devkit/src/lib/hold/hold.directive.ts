e3e78455c27a6381d20565e6cd1c012c265447fc
joker876 <joker876x@gmail.com>
2023-04-14 22:09:39 +0200

    [devkit] allow to trigger hold directive using the keyboard

--- projects/devkit/src/lib/hold/hold.directive.ts
+++ projects/devkit/src/lib/hold/hold.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { coerceBooleanProperty } from '../coercion/boolean';
 
 /**
   Detects when the user clicks-and-holds a given element.
@@ -13,7 +14,18 @@ export class HoldDirective {
     @Input() set readonly(v: any) { this._clear(); }
 
     @Input('ardHoldDelay') holdDelay: number = 500;
-    @Input('ardHoldRepeat') holdRepeat: number = 1000/15;
+    @Input('ardHoldRepeat') holdRepeat: number = 1000 / 15;
+    
+    private _allowSpaceKey: boolean = false;
+    @Input('ardHoldSpaceKey')
+    get allowSpaceKey(): boolean { return this._allowSpaceKey; }
+    set allowSpaceKey(v: any) { this._allowSpaceKey = coerceBooleanProperty(v); }
+    
+    private _allowEnterKey?: boolean = undefined;
+    @Input('ardHoldEnterKey')
+    get allowEnterKey(): boolean { return this._allowEnterKey ?? this.allowSpaceKey; }
+    set allowEnterKey(v: any) { this._allowEnterKey = coerceBooleanProperty(v); }
+    
 
     interval: NodeJS.Timeout | null = null;
     timeout: NodeJS.Timeout | null = null;
@@ -44,5 +56,32 @@ export class HoldDirective {
 
         clearInterval(this.interval);
         this.interval = null;
+    }
+
+    isKeyDown: boolean = false;
+
+    @HostListener('keydown', ['$event'])
+    public onKeyDown(event: KeyboardEvent): void {
+        if (this.allowEnterKey && event.code == 'Enter') event.preventDefault();
+        if (this.isKeyDown) return;
+        console.log('onKeyDown', this.allowEnterKey, event.code);
+        if (
+            this.allowSpaceKey && event.code == 'Space' ||
+            this.allowEnterKey && event.code == 'Enter'
+        ) {
+            console.log('triggering');
+            this.onMouseDown();
+            this.isKeyDown = true;
+        }
+    }
+    @HostListener('keyup', ['$event'])
+    public onKeyUp(event: KeyboardEvent): void {
+        this.isKeyDown = false;
+        if (
+            this.allowSpaceKey && event.code == 'Space' ||
+            this.allowEnterKey && event.code == 'Enter'
+        ) {
+            this.onMouseUp();
+        }
     }
 }
