bd779eac28613cbe418ba84980977c3bc8f24e4a
joker876 <joker876x@gmail.com>
2023-04-28 20:59:45 +0200

    [playground] color picker page

--- projects/playground/src/app/pages/color-picker/color-picker.page.ts
+++ projects/playground/src/app/pages/color-picker/color-picker.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Logger } from '../../services/logger.service';
 
 @Component({
   selector: 'app-color-picker',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ColorPickerPage {
 
+    constructor(private _logger: Logger) { }
+    readonly log = this._logger.log;
 }
