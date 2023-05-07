4e9cce1e124d930111dc942553c5a5fd682efc60
joker876 <joker876x@gmail.com>
2023-05-07 21:49:04 +0200

    escape HTML in all labels in select component

--- projects/ui/src/lib/select/select.module.ts
+++ projects/ui/src/lib/select/select.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { ArdiumChipModule } from './../chip/chip.module';
 
+import { ArdiumClickOutsideEventModule } from 'projects/devkit/src/public-api';
+import { ArdiumDropdownPanelModule } from '../dropdown-panel/dropdown-panel.module';
+import { ArdiumSelectComponent } from './select.component';
 import {
     ArdDropdownFooterTemplateDirective,
     ArdDropdownHeaderTemplateDirective,
+    ArdItemDisplayLimitTemplateDirective,
+    ArdItemLimitReachedTemplateDirective,
     ArdLoadingPlaceholderTemplateDirective,
     ArdLoadingSpinnerTemplateDirective,
     ArdNoItemsFoundTemplateDirective,
@@ -13,14 +18,10 @@ import {
     ArdOptionTemplateDirective,
     ArdSelectPlaceholderTemplateDirective,
     ArdValueChipTemplateDirective,
-    ArdValueTemplateDirective,
-    ArdItemLimitReachedTemplateDirective,
-    ArdItemDisplayLimitTemplateDirective,
+    ArdValueTemplateDirective
 } from './select.directive';
-import { ArdiumDropdownPanelModule } from '../dropdown-panel/dropdown-panel.module';
-import { ArdiumClickOutsideEventModule } from 'projects/devkit/src/public-api';
-import { ArdiumSelectComponent } from './select.component';
 
+import { ArdiumEscapeHTMLModule } from '../../../../devkit/src/lib/escape-html/escape-html.module';
 import { _ClearButtonModule } from './../_internal/clear-button/clear-button.module';
 
 @NgModule({
@@ -41,11 +42,12 @@ import { _ClearButtonModule } from './../_internal/clear-button/clear-button.mod
         ArdItemDisplayLimitTemplateDirective,
     ],
     imports: [
-        CommonModule,
+    CommonModule,
         ArdiumDropdownPanelModule,
         ArdiumChipModule,
         ArdiumClickOutsideEventModule,
         _ClearButtonModule,
+        ArdiumEscapeHTMLModule,
     ],
     exports: [
         ArdiumSelectComponent,
