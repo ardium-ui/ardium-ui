7fc85bf307a1e5f9e6fad57c8dcb451c661e583a
joker876 <joker876x@gmail.com>
2023-05-13 15:45:35 +0200

    start implementing color picker inputs

--- projects/ui/src/lib/color/color-picker/color-picker.module.ts
+++ projects/ui/src/lib/color/color-picker/color-picker.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ArdiumHexInputModule } from '../../inputs/hex-input/hex-input.module';
+import { ArdiumNumberInputModule } from '../../inputs/number-input/number-input.module';
+import { ArdiumOptionModule } from '../../option/option.module';
+import { ArdiumSelectModule } from '../../select/select.module';
 import { ArdiumColorPickerComponent } from './color-picker.component';
 import { ArdColorPickerColorWindowTemplateDirective, ArdColorPickerHueIndicatorTemplateDirective, ArdColorPickerOpacityIndicatorTemplateDirective, ArdColorPickerShadeIndicatorTemplateDirective } from './color-picker.directives';
 
@@ -14,7 +18,11 @@ import { ArdColorPickerColorWindowTemplateDirective, ArdColorPickerHueIndicatorT
         ArdColorPickerOpacityIndicatorTemplateDirective,
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ArdiumSelectModule,
+        ArdiumOptionModule,
+        ArdiumNumberInputModule,
+        ArdiumHexInputModule,
     ],
     exports: [
         ArdiumColorPickerComponent,
