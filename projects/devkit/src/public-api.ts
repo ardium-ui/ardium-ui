dc1df080d063a2e001adb5a6049c0778519a8af5
joker876 <joker876x@gmail.com>
2023-06-07 16:02:26 +0200

    [devkit] add function "coerceDateProperty"

--- projects/devkit/src/public-api.ts
+++ projects/devkit/src/public-api.ts
@@ -18,6 +18,7 @@ export * from "./lib/hold/hold.directive";
 export * from "./lib/coercion/boolean";
 export * from "./lib/coercion/number";
 export * from "./lib/coercion/array";
+export * from "./lib/coercion/date";
 
 export * from './lib/find-functions/find-suggestion';
 
