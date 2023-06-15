4032f3ae4f78637b6e1886a2f83e5811cef88d25
joker876 <joker876x@gmail.com>
2023-06-15 23:42:34 +0200

    change select directive selectors

--- projects/ui/src/lib/select/select.directive.ts
+++ projects/ui/src/lib/select/select.directive.ts
@@ -4,77 +4,77 @@ import { CustomOptionContext, GroupContext, ItemDisplayLimitContext, ItemLimitCo
 
 //public (exported)
 
-@Directive({ selector: 'ng-template[ard-prefix-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-prefix-tmp]' })
 export class ArdSelectPrefixTemplateDirective {
     constructor(public template: TemplateRef<undefined>) { }
 }
 
-@Directive({ selector: 'ng-template[ard-suffix-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-suffix-tmp]' })
 export class ArdSelectSuffixTemplateDirective {
     constructor(public template: TemplateRef<undefined>) { }
 }
 
-@Directive({ selector: 'ng-template[ard-value-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-value-tmp]' })
 export class ArdValueTemplateDirective {
     constructor(public template: TemplateRef<ValueContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-value-chip-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-value-chip-tmp]' })
 export class ArdValueChipTemplateDirective {
     constructor(public template: TemplateRef<ValueContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-option-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-option-tmp]' })
 export class ArdOptionTemplateDirective {
     constructor(public template: TemplateRef<OptionContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-optgroup-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-optgroup-tmp]' })
 export class ArdOptgroupTemplateDirective {
     constructor(public template: TemplateRef<GroupContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-placeholder-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-placeholder-tmp]' })
 export class ArdSelectPlaceholderTemplateDirective {
     constructor(public template: TemplateRef<PlaceholderContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-loading-spinner-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-loading-spinner-tmp]' })
 export class ArdLoadingSpinnerTemplateDirective {
     constructor(public template: TemplateRef<null>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-loading-placeholder-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-loading-placeholder-tmp]' })
 export class ArdLoadingPlaceholderTemplateDirective {
     constructor(public template: TemplateRef<SearchContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-dropdown-header-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-dropdown-header-tmp]' })
 export class ArdDropdownHeaderTemplateDirective {
     constructor(public template: TemplateRef<SearchContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-dropdown-footer-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-dropdown-footer-tmp]' })
 export class ArdDropdownFooterTemplateDirective {
     constructor(public template: TemplateRef<SearchContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-no-items-found-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-no-items-found-tmp]' })
 export class ArdNoItemsFoundTemplateDirective {
     constructor(public template: TemplateRef<SearchContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-add-custom-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-add-custom-tmp]' })
 export class ArdAddCustomTemplateDirective {
     constructor(public template: TemplateRef<CustomOptionContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-item-limit-reached-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-item-limit-reached-tmp]' })
 export class ArdItemLimitReachedTemplateDirective {
     constructor(public template: TemplateRef<ItemLimitContext>) {}
 }
 
-@Directive({ selector: 'ng-template[ard-item-display-limit-tmp]' })
+@Directive({ selector: 'ard-select > ng-template[ard-item-display-limit-tmp]' })
 export class ArdItemDisplayLimitTemplateDirective {
     constructor(public template: TemplateRef<ItemDisplayLimitContext>) {}
 }
