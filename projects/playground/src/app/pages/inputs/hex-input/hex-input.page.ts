2d79b8d5eabc987a2b0c5ce0938a908e940e7d8a
joker876 <joker876x@gmail.com>
2023-06-16 14:03:55 +0200

    [playground] fix hex-input page bug
    in the example with current value displayed, the hash sign was sometimes displayed twice

--- projects/playground/src/app/pages/inputs/hex-input/hex-input.page.ts
+++ projects/playground/src/app/pages/inputs/hex-input/hex-input.page.ts
@@ -8,7 +8,7 @@ import { FormElementAppearance as FA, FormElementVariant as FV } from '@ardium-u
   styleUrls: ['./hex-input.page.scss']
 })
 export class HexInputPage {
-    value: string | null = '000000';
+    value: string | null = '#000000';
 
     appearances: FA[] = Object.values(FA);
     variants: FV[] = Object.values(FV);
