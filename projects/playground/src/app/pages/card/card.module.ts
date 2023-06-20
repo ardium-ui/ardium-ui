06410539f2064acbf0ba2d85a645022ac3cf432a
joker876 <joker876x@gmail.com>
2023-06-20 16:37:46 +0200

    [playground] improve card page

--- projects/playground/src/app/pages/card/card.module.ts
+++ projects/playground/src/app/pages/card/card.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardPage } from './card.page';
+import { ArdiumCardModule, ArdiumButtonModule, ArdiumFabModule } from '@ardium-ui/ui';
 
 
 
--- projects/playground/src/app/pages/card/card.page.ts
+++ projects/playground/src/app/pages/card/card.page.ts
@@ -8,4 +8,9 @@ import { OneAxisAlignment } from '@ardium-ui/ui';
     encapsulation: ViewEncapsulation.None,
 })
 export class CardPage {
+    bearText = 'The brown bear is a large bear living in much of northern Eurasia and North America. It is smaller than the polar bear, but is the largest carnivore which lives entirely on the land. There are several recognized subspecies.'
+
+    aligns: OneAxisAlignment[] = Object.values(OneAxisAlignment);
+
+    likesBear: boolean = false;
 }
