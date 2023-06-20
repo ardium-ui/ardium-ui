8d6244285c07a9f847962d1f200f102dddf490cf
joker876 <joker876x@gmail.com>
2023-06-20 16:37:59 +0200

    [playground] cleanup app.module.ts

--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -9,21 +9,21 @@ import { CalendarModule } from './pages/calendar/calendar.module';
 import { CardModule } from './pages/card/card.module';
 import { CheckboxModule } from './pages/checkbox/checkbox.module';
 import { ChipsModule } from './pages/chips/chips.module';
+import { ColorDisplayModule } from './pages/color-display/color-display.module';
+import { ColorPickerModule } from './pages/color-picker/color-picker.module';
 import { FabModule } from './pages/fab/fab.module';
 import { HomeModule } from './pages/home/home.module';
 import { IconModule } from './pages/icon/icon.module';
 import { InputsModule } from './pages/inputs/inputs.module';
+import { KbdShortcutModule } from './pages/kbd-shortcut/kbd-shortcut.module';
+import { KbdModule } from './pages/kbd/kbd.module';
+import { ProgressCircleModule } from './pages/progress-circle/progress-circle.module';
 import { SegmentModule } from './pages/segment/segment.module';
 import { SelectModule } from './pages/select/select.module';
 import { SlideToggleModule } from './pages/slide-toggle/slide-toggle.module';
 import { SlidersModule } from './pages/sliders/sliders.module';
 import { StarsModule } from './pages/stars/stars.module';
 import { StateboxModule } from './pages/statebox/statebox.module';
-import { ColorDisplayModule } from './pages/color-display/color-display.module';
-import { ColorPickerModule } from './pages/color-picker/color-picker.module';
-import { ProgressCircleModule } from './pages/progress-circle/progress-circle.module';
-import { KbdModule } from './pages/kbd/kbd.module';
-import { KbdShortcutModule } from './pages/kbd-shortcut/kbd-shortcut.module';
 
 @NgModule({
     declarations: [
