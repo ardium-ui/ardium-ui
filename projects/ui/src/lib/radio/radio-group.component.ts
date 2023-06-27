6bdd0c448283dbb89a6ad7fe37f56b317695f660
joker876 <joker876x@gmail.com>
2023-06-27 14:12:50 +0200

    finish radio & radio-group

--- projects/ui/src/lib/radio/radio-group.component.ts
+++ projects/ui/src/lib/radio/radio-group.component.ts
@@ -1,12 +1,187 @@
-import { ChangeDetectionStrategy, Component, ContentChildren, ViewEncapsulation, QueryList, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ContentChildren, forwardRef, Input, OnDestroy, OnInit, QueryList, ViewEncapsulation, AfterContentInit, Output, EventEmitter } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { _NgModelComponentBase } from '../_internal/ngmodel-component';
 import { ArdiumRadioComponent } from './radio/radio.component';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
     selector: 'ard-radio-group',
     template: '<ng-content></ng-content>',
-    styleUrls: ['./radio-group.component.scss'],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        '[attr.tabindex]': 'null',
+        '[attr.aria-label]': 'null',
+        '[attr.aria-labelledby]': 'null',
+        '[attr.aria-describedby]': 'null',
+        'role': 'radiogroup',
+    },
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => ArdiumRadioGroupComponent),
+            multi: true
+        }
+    ]
 })
-export class ArdiumRadioGroupComponent {
+export class ArdiumRadioGroupComponent extends _NgModelComponentBase implements AfterContentInit, AfterViewInit, OnDestroy {
+    @ContentChildren(ArdiumRadioComponent, { descendants: true })
+    private _radios!: QueryList<ArdiumRadioComponent>;
+
+    constructor(
+    ) {
+        super();
+    }
+
+    //! value
+    @Input()
+    get value(): any { return this._selected?.value; }
+    set value(v: any) {
+        this.writeValue(v);
+    }
+
+    @Output() valueChange = new EventEmitter<any>();
+    @Output('change') changeEvent = new EventEmitter<any>();
+
+    writeValue(v: any): void {
+        if (this.value !== v) {
+
+            this._findRadioByValue(v);
+        }
+        this._updateRadiosByValue();
+    }
+
+    private _findRadioByValue(v: any): void {
+        if (!this._radios) return;
+
+        this._selected = this._radios.find(radio => this.value === radio.value) ?? null;
+    }
+
+    /** Updates all child radios depending on the currently selected radio. */
+    private _updateRadiosByValue(): void {
+        if (!this._radios) return;
+        
+        this._radios.forEach(radio => {
+            radio.selected = this.value === radio.value;
+            radio.markForCheck();
+        });
+    }
+
+    private _selected: ArdiumRadioComponent | null = null;
+    /**
+     * The currently selected radio button. If set to a new radio button, the radio group value
+     * will be updated to match the new selected button.
+     */
+    @Input()
+    get selected() {
+        return this._selected;
+    }
+    set selected(selected: ArdiumRadioComponent | null) {
+        this._selected = selected;
+        this._checkSelectedRadioButton();
+    }
+
+    private _checkSelectedRadioButton() {
+        if (this._selected && !this._selected.selected) {
+            this._selected.selected = true;
+        }
+    }
+
+    //! name
+    private _name: string = crypto.randomUUID();
+    /** Name of the radio button group. All radio buttons inside this group will use this name. */
+    @Input()
+    get name(): string {
+        return this._name;
+    }
+    set name(value: string) {
+        this._name = value;
+        this._updateRadioButtonNames();
+    }
+
+
+    private _updateRadioButtonNames(): void {
+        if (this._radios) {
+            this._radios.forEach(radio => {
+                radio.name = this.name;
+                radio.markForCheck();
+            });
+        }
+    }
+
+    //! update handlers
+    private _handleBlurEvents(event: FocusEvent): void {
+        this.onBlur(event);
+    }
+    private _handleFocusEvents(event: FocusEvent): void {
+        this.onFocus(event);
+    }
+    private _handleChangeEvents(selected: ArdiumRadioComponent): void {
+        this.selected = selected;
+        this.writeValue(selected.value);
+
+        this._emitChange();
+    }
+
+    private _emitChange(): void {
+        const v = this.value;
+        this._onChangeRegistered?.(v);
+        this.changeEvent.emit(v);
+        this.valueChange.emit(v);
+    }
+
+    //! hooks
+    private readonly _subscriptions: Subscription[] = [];
+    private _childEventSubs: Subscription[] = [];
+    ngAfterContentInit(): void {
+        this._updateRadioButtonNames();
+
+        this._radios.forEach(radio => {
+            this._childEventSubs.push(radio.blurEvent.subscribe(v => {
+                this._handleBlurEvents(v);
+            }));
+            this._childEventSubs.push(radio.focusEvent.subscribe(v => {
+                this._handleFocusEvents(v);
+            }));
+            this._childEventSubs.push(radio.selectedChange.subscribe(() => {
+                this._handleChangeEvents(radio);
+            }));
+        });
+    }
+    ngAfterViewInit(): void {
+        const sub = (this._radios.changes as Observable<ArdiumRadioComponent[]>).subscribe(radios => {
+            setTimeout(() => {
+                this._updateRadioButtonNames();
+            }, 0);
+
+            this._destroyChildSubscriptions();
+
+            //sub to child component events
+            for (const radio of radios) {
+                this._childEventSubs.push(radio.blurEvent.subscribe(v => {
+                    this._handleBlurEvents(v);
+                }));
+                this._childEventSubs.push(radio.focusEvent.subscribe(v => {
+                    this._handleFocusEvents(v);
+                }));
+                this._childEventSubs.push(radio.selectedChange.subscribe(() => {
+                    this._handleChangeEvents(radio);
+                }));
+            }
+        });
+
+        this._subscriptions.push(sub);
+    }
+    ngOnDestroy(): void {
+        for (const sub of this._subscriptions) {
+            sub.unsubscribe();
+        }
+        this._destroyChildSubscriptions();
+    }
+    private _destroyChildSubscriptions(): void {
+        for (const sub of this._childEventSubs) {
+            sub.unsubscribe();
+        }
+        this._childEventSubs = [];
+    }
 }
--- projects/ui/src/lib/radio/radio/radio.component.ts
+++ projects/ui/src/lib/radio/radio/radio.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 import { ComponentColor } from '@ardium-ui/ui';
 import { _BooleanComponentBase } from '../../_internal/boolean-component';
 
@@ -7,9 +7,18 @@ import { _BooleanComponentBase } from '../../_internal/boolean-component';
     templateUrl: './radio.component.html',
     styleUrls: ['./radio.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArdiumRadioComponent extends _BooleanComponentBase {
+
+    @ViewChild('radio') private _radioEl!: ElementRef<HTMLInputElement>;
+
+    constructor(
+        protected _changeDetector: ChangeDetectorRef,
+    ) {
+        super();
+    }
+
     @Input() htmlId: string = crypto.randomUUID();
 
     @Input() value: any;
@@ -24,6 +33,35 @@ export class ArdiumRadioComponent extends _BooleanComponentBase {
         ].join(' ');
     }
 
+    //! event handlers
+    onMousedown(): void {
+        this.focus();
+    }
+    onMouseup(): void {
+        this.focus();
+        
+        if (this.selected) return;
+        this.selected = true;
+
+        this._emitChange();
+    }
+
+    log(...args: any[]): void {
+        console.log(...args);
+    }
+
     //! radio-group access points
     name: string | null = null;
+
+    //! helpers
+    /**
+     * Marks the radio button as needing checking for change detection.
+     * This method is exposed because the parent radio group will directly
+     * update bound properties of the radio button.
+     */
+    markForCheck() {
+        // When group value changes, the button will not be notified. Use `markForCheck` to explicit
+        // update radio button's status
+        this._changeDetector.markForCheck();
+    }
 }
