5b2a2f01990e81d9043afcb23d8308df3fffb0cd
joker876 <joker876x@gmail.com>
2023-07-04 15:24:23 +0200

    [devkit] improve filesize pipe

--- projects/devkit/src/lib/filesize/filesize.pipe.ts
+++ projects/devkit/src/lib/filesize/filesize.pipe.ts
@@ -2,12 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { approximate } from 'more-rounding';
 
 const filesizeMap = {
-    k: 'kB',
-    M: 'MB',
-    B: 'GB',
-    T: 'TB',
-    Qa: 'PB',
-    Qi: 'EB',
+    k: 'k',
+    M: 'M',
+    B: 'G',
+    T: 'T',
+    Qa: 'P',
+    Qi: 'E',
 }
 
 @Pipe({
@@ -16,6 +16,6 @@ const filesizeMap = {
 export class ArdiumFilesizePipe implements PipeTransform {
 
     transform(value: number, precision: number = 2): string {
-        return approximate(value, precision, undefined, undefined, filesizeMap);
+        return approximate(value, precision, undefined, undefined, filesizeMap) + "B";
     }
 }
