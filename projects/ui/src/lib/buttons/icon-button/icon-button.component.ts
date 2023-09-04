8038fd984971368d54212d12d08d51fd53ea6fb7
joker876 <joker876x@gmail.com>
2023-09-04 21:38:52 +0200

    create icon button

---
+++ projects/ui/src/lib/buttons/icon-button/icon-button.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ComponentColor } from '../../types/colors.types';
+import { _FocusableComponentBase } from '../../_internal/focusable-component';
+import { ButtonAppearance, FABSize } from '../general-button.types';
+import { coerceBooleanProperty } from '@ardium-ui/devkit';
+
+@Component({
+  selector: 'ard-icon-button',
+  templateUrl: './icon-button.component.html',
+    styleUrls: ['./icon-button.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ArdiumIconButtonComponent extends _FocusableComponentBase {
+
+    @Input() wrapperClasses: string = '';
+
+    //! button settings
+    @Input() color: ComponentColor = ComponentColor.Primary;
+
+    private _compact: boolean = false;
+    @Input()
+    get compact(): boolean { return this._compact; }
+    set compact(v: any) { this._compact = coerceBooleanProperty(v); }
+
+    get ngClasses(): string {
+        return [
+            'ard-appearance-transparent',
+            `ard-color-${this.color}`,
+            this.compact ? 'ard-compact' : '',
+        ].join(' ');
+    }
+}
---
+++ projects/ui/src/lib/buttons/icon-button/icon-button.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArdiumIconButtonComponent } from './icon-button.component';
+
+
+
+@NgModule({
+    declarations: [
+        ArdiumIconButtonComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        ArdiumIconButtonComponent
+    ]
+})
+export class ArdiumIconButtonModule { }
