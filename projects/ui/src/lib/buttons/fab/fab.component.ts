eb0847ef70b9bd0387280fe69627f5d28d1aec93
joker876 <joker876x@gmail.com>
2023-09-07 19:12:39 +0200

    implement extended FAB

--- projects/ui/src/lib/buttons/fab/fab.component.ts
+++ projects/ui/src/lib/buttons/fab/fab.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 import { ComponentColor } from '../../types/colors.types';
 import { _FocusableComponentBase } from '../../_internal/focusable-component';
 import { ButtonAppearance, FABSize } from '../general-button.types';
+import { coerceBooleanProperty } from '@ardium-ui/devkit';
 
 @Component({
   selector: 'ard-fab',
@@ -19,7 +20,10 @@ export class ArdiumFabComponent extends _FocusableComponentBase {
     @Input() size: FABSize = FABSize.Standard;
     @Input() color: ComponentColor = ComponentColor.Primary;
 
-    @Input() icon?: string;
+    private _extended: boolean = false;
+    @Input()
+    get extended(): boolean { return this._extended; }
+    set extended(v: any) { this._extended = coerceBooleanProperty(v); }
 
     //* for adding classes to the button
     get ngClasses(): string {
@@ -27,6 +31,7 @@ export class ArdiumFabComponent extends _FocusableComponentBase {
             `ard-appearance-${this.appearance}`,
             `ard-color-${this.color}`,
             `ard-fab-size-${this.size}`,
+            this.extended ? 'ard-fab-extended' : '',
         ].join(' ');
     }
 }
