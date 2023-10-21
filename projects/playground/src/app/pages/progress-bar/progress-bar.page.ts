78c6715ae82d7547fb20dcb67b0fa6b12ddcc71a
joker876 <joker876x@gmail.com>
2023-10-21 21:41:44 +0200

    [playground] improve progress bar page

--- projects/playground/src/app/pages/progress-bar/progress-bar.page.ts
+++ projects/playground/src/app/pages/progress-bar/progress-bar.page.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.page.html',
   styleUrls: ['./progress-bar.page.scss']
 })
-export class ProgressBarPage {
+export class ProgressBarPage implements OnInit {
 
+
+    currentProgress = -17;
+    currentBuffer = -7;
+
+    private _createTimeout(fn: (modif: number) => void) {
+        const randomModifier = 200 + Math.random() * 300;
+        setTimeout(() => {
+            fn(randomModifier);
+            this._createTimeout(fn);
+        }, randomModifier);
+    }
+
+    ngOnInit(): void {
+        this._createTimeout(modif => {
+            if (this.currentProgress > 100) {
+                this.currentProgress = -17;
+                this.currentBuffer = -7;
+            }
+            this.currentProgress += (1000 + Math.random() * 8000) * modif / 1_000_000;
+        });
+        this._createTimeout(modif => {
+            this.currentBuffer += (1000 + Math.random() * 7500) * modif / 1_000_000 * 1.1;
+        })
+    }
 }
