bb28f1ec8e5c1820f8a4e5c0c148028cf766b02d
joker876 <joker876x@gmail.com>
2023-11-16 21:04:44 +0100

    [playground] add more digit-input cases

--- projects/playground/src/app/pages/inputs/digit-input/digit-input.page.ts
+++ projects/playground/src/app/pages/inputs/digit-input/digit-input.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Logger } from '../../../services/logger.service';
+import { DigitInputConfig, TransformType } from '@ardium-ui/ui';
 
 @Component({
   selector: 'app-digit-input',
@@ -10,4 +11,25 @@ export class DigitInputPage {
 
     constructor(private _logger: Logger) {}
     log = this._logger.log;
+
+    readonly simpleConfig = 'letter,number,number,number';
+
+    readonly complexConfig: DigitInputConfig = [
+        {
+            accept: 'a-f0-9',
+            transform: TransformType.Uppercase,
+        },
+        {
+            accept: 'a-f0-9',
+            transform: TransformType.Uppercase,
+        },
+        {
+            accept: 'a-f0-9',
+            transform: TransformType.Uppercase,
+        },
+        {
+            accept: 'a-f0-9',
+            transform: TransformType.Uppercase,
+        },
+    ]
 }
