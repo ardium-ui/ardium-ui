aef085d325651ec216ea95b86d7cc0058db8aa83
joker876 <joker876x@gmail.com>
2024-03-21 19:24:36 +0100

    [playground] improve snackbar page

--- projects/playground/src/app/pages/snackbar/snackbar.page.ts
+++ projects/playground/src/app/pages/snackbar/snackbar.page.ts
@@ -1,18 +1,29 @@
 import { Component, inject } from '@angular/core';
-import { ArdiumSnackbarService } from '@ardium-ui/ui';
+import { ArdSnackbarQueueHandling, ArdSnackbarRef, ArdSnackbarType, ArdiumSnackbarService } from '@ardium-ui/ui';
 
 @Component({
-  selector: 'app-snackbar',
-  templateUrl: './snackbar.page.html',
-  styleUrl: './snackbar.page.scss'
+    selector: 'app-snackbar',
+    templateUrl: './snackbar.page.html',
+    styleUrl: './snackbar.page.scss',
 })
 export class SnackbarPage {
     private readonly _snackbarService = inject(ArdiumSnackbarService);
 
     openSnackbar1() {
-        const ref = this._snackbarService.open('Hello world!');
+        this._snackbarService.open('Hello world!');
     }
     openSnackbar2() {
-        const ref = this._snackbarService.open('Hello world!', 'Dismiss');
+        this._snackbarService.open('Hello world!', 'Dismiss');
+    }
+    openSnackbar3(type: ArdSnackbarType) {
+        this._snackbarService.open('Hello world!', 'Dismiss', { type, queueHandling: ArdSnackbarQueueHandling.Overwrite });
+    }
+
+    private _ref4?: ArdSnackbarRef;
+    openSnackbar4() {
+        this._ref4 = this._snackbarService.open('Hello world!', 'Dismiss', { duration: Infinity, queueHandling: ArdSnackbarQueueHandling.Overwrite });
+    }
+    closeSnackbar4() {
+        this._ref4?.close();
     }
 }
