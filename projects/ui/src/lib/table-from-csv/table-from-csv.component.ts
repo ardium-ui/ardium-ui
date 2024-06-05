ecd18689c82039ab4d329eef6ddbc2413fbe4714
joker876 <joker876x@gmail.com>
2024-06-05 21:18:33 +0200

    convert table-from-csv

--- projects/ui/src/lib/table-from-csv/table-from-csv.component.ts
+++ projects/ui/src/lib/table-from-csv/table-from-csv.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, Input, AfterContentInit } from '@angular/core';
+import { AfterContentInit, ChangeDetectionStrategy, Component, ViewEncapsulation, computed, input } from '@angular/core';
 import { TableDataColumn } from '../table/table.types';
-import { NonEmptyArray } from '../types/utility.types';
 
 @Component({
   selector: 'ard-table-from-csv',
@@ -9,27 +8,24 @@ import { NonEmptyArray } from '../types/utility.types';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ArdiumTableFromCsvComponent implements AfterContentInit {
-  @Input() separator: string = ',';
+  readonly separator = input<string>(',');
 
-  private _isDataOkay: boolean = false;
-  @Input()
-  set data(v: string) {
-    this._isDataOkay = false;
-    if (!this._isCsvValid(v)) return;
-    const lines = v.split('\n');
-    const headers = this._convertCsvToHeaders(lines.shift()!);
-    const dataRows = this._convertCsvToArray(lines, headers);
+  readonly data = input<{ headers: TableDataColumn[]; dataRows: any[] } | null, string>(null, {
+    transform: v => {
+      if (!this._isCsvValid(v)) return null;
+      const lines = v.split('\n');
+      const headers = this._convertCsvToHeaders(lines.shift()!);
+      const dataRows = this._convertCsvToArray(lines, headers);
+      return { headers, dataRows }
+    },
+  });
+  private readonly _isDataOkay = computed(() => this.data() !== null);
 
-    this._isDataOkay = true;
-    this.headers = headers;
-    this.dataRows = dataRows;
-  }
-
-  headers: TableDataColumn[] = [];
-  dataRows: any[] = [];
+  readonly headers = computed(() => this.data()?.headers ?? []);
+  readonly dataRows = computed(() => this.data()?.dataRows ?? []);
 
   ngAfterContentInit(): void {
-    if (!this._isDataOkay) {
+    if (!this._isDataOkay()) {
       console.error(new Error('<ard-table-from-csv> error: must provide non-empty data')); //TODO
     }
   }
@@ -38,7 +34,7 @@ export class ArdiumTableFromCsvComponent implements AfterContentInit {
     return csv.length > 1;
   }
   private _convertCsvToHeaders(line: string): TableDataColumn[] {
-    const headers = line.split(this.separator);
+    const headers = line.split(this.separator());
     return headers.map(header => {
       header = header.trim();
       return {
@@ -51,7 +47,7 @@ export class ArdiumTableFromCsvComponent implements AfterContentInit {
     const dataRows: { [key: string]: any }[] = [];
 
     for (const line of lines) {
-      const lineArr = line.split(this.separator);
+      const lineArr = line.split(this.separator());
 
       const obj: { [key: string]: any } = {};
       for (let i = 0; i < headers.length; i++) {
