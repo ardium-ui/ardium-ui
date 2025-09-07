e598e81b712d10ec1064ab105693720f4ba3168a
joker876 <joker876x@gmail.com>
2025-09-07 18:36:00 +0200

    fix public-api

--- projects/ui/src/lib/form-field/index.ts
+++ projects/ui/src/lib/form-field/index.ts
@@ -1,8 +1,10 @@
 export * from './error/error.component';
 export * from './error/error.directive';
+export * from './form-field-native-inputs';
 export * from './form-field.component';
 export * from './form-field.module';
 export * from './hint/hint.component';
 export * from './hint/hint.directive';
 export * from './horizontal-form-field.component';
 export * from './label/label.component';
+
