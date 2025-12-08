1dd574a64585ba3d8e361527b5cb2f82a6576454
joker876 <joker876x@gmail.com>
2025-12-08 15:36:21 +0100

    export all relevant files from tabber

--- projects/ui/src/lib/tabber/index.ts
+++ projects/ui/src/lib/tabber/index.ts
@@ -2,4 +2,7 @@ export * from './tab/tab.component';
 export * from './tab/tab.defaults';
 export * from './tabber.component';
 export * from './tabber.defaults';
+export * from './tabber.directives';
 export * from './tabber.module';
+export * from './tabber.types';
+
