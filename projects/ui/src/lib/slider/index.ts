5179972272b527e0883ee21ee08f463ca811be8e
joker876 <joker876x@gmail.com>
2026-01-05 14:59:36 +0100

    fix slider index.ts

--- projects/ui/src/lib/slider/index.ts
+++ projects/ui/src/lib/slider/index.ts
@@ -1,7 +1,9 @@
 export * from './range-slider/range-slider.component';
 export * from './range-slider/range-slider.module';
+export * from './range-slider/range-slider.types';
 export * from './slider.component';
 export * from './slider.defaults';
 export * from './slider.directive';
 export * from './slider.module';
 export * from './slider.types';
+
